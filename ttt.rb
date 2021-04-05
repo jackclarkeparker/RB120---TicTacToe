0a6e088e258d5f67f844999c2876c07bf5674cc6
Jack Clarke-Parker <jackclarkeparker@gmail.com>
2021-04-06 08:37:12 +1200

    renamed to ttt

--- ttt.rb
+++ ttt.rb
@@ -182,6 +182,7 @@ end
 class TTTGame
   HUMAN_MARKER = 'X'
   COMPUTER_MARKER = 'O'
+  FIRST_TO_MOVE = HUMAN_MARKER
 
   attr_reader :board, :human, :computer
 
@@ -189,6 +190,21 @@ class TTTGame
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
+    @current_marker = FIRST_TO_MOVE
+  end
+
+  def current_player_moves
+    if human_turn?
+      human_moves
+      @current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_marker = HUMAN_MARKER
+    end
+  end
+
+  def human_turn?
+    @current_marker == HUMAN_MARKER
   end
 
   def human_moves
@@ -238,13 +254,9 @@ class TTTGame
       display_board
 
       loop do
-        human_moves
+        current_player_moves
         break if board.someone_won? || board.full?
-
-        computer_moves
-        break if board.someone_won? || board.full?
-
-        clear_screen_and_display_board
+        clear_screen_and_display_board if human_turn?
       end
       display_result
       break unless play_again?
@@ -257,6 +269,7 @@ class TTTGame
 
   def reset
     clear
+    @current_marker = FIRST_TO_MOVE
     board.reset
   end
 
